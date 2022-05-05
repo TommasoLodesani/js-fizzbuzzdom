f4bebfd2ab5db313df2eaef1437a8f27ad7d393b
Tommaso <tommaso.lodesani@gmail.com>
2022-05-05 15:10:36 +0200

    if

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
 // programma che stampi in log numeri da 1 a 100
+let i;
 
-// Per i multipli di 3 stampi “Fizz” al posto del numero
+for (let i = 1 ; i <101; i++){
+    console.log(i);
+    
+    if (i % 3 == 0){
+        console.log("Fizz");
+    } else if (i % 5 == 0){
+        console.log("Buzz");
 
+    } else if (i % 15 == 0){
+        console.log("FizzBuzz");
+    }
+    
+
+    // Per i multipli di 3 stampi “Fizz” al posto del numero
+   
+
+}
 // Per i multipli di 5 stampi Buzz
 
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
