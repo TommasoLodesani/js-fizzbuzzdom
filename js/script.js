e29c60dfad163b8c6d05c79cb9c37d877eea6af9
Tommaso <tommaso.lodesani@gmail.com>
2022-06-07 17:05:50 +0200

    js

--- js/script.js
+++ js/script.js
@@ -4,23 +4,26 @@ const mySquares = document.querySelector(".my_container");
 
 // programma che stampi in log numeri da 1 a 100
 for (let i = 1 ; i < 101; i++){
-    let element;
 
     if (i % 3 == 0 && i % 5 == 0){
-        element = `<div class = "square fizzBuzz">${i}</div>`;
+        element = `<div class = "square fizzBuzz">FizzBuzz</div>`;
+        mySquares.innerHTML += element;
         console.log("FizzBuzz");
     } else if (i % 3 == 0){
-        element = `<div class = "square fizz">${i}</div>`;
+        element = `<div class = "square fizz">Fizz</div>`;
+        mySquares.innerHTML += element;
         console.log("Fizz");
     } else if (i % 5 == 0){
-        element = `<div class = "square buzz">${i}</div>`;
+        element = `<div class = "square buzz">Buzz</div>`;
+        mySquares.innerHTML += element;
         console.log("Buzz");
     } else{
         element = `<div class = "square normal">${i}</div>`;
+        mySquares.innerHTML += element;
         console.log(i);
     }
 
-    mySquares.innerHTML += element;
+    
 
 
     
